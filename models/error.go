aa297b1ff9d5f48de77943fb901eb0f3c9b27ccf
afandy2000 <afandywibowo2000@gmail.com>
2022-10-09 17:53:44 +0800

    feat: costum error

---
+++ models/error.go
@@ -0,0 +1,61 @@
+package models
+
+import "net/http"
+
+type Errors struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Errors  interface{} `json:"errors"`
+}
+
+func (e *Errors) Error() string {
+	return e.Message
+}
+
+func NewErrors(code int, msg string, data interface{}) *Errors {
+	return &Errors{
+		Code:    code,
+		Message: msg,
+		Errors:  data,
+	}
+}
+
+func NewBadRequest(data interface{}) *Errors {
+	return &Errors{
+		Code:    http.StatusBadRequest,
+		Message: "bad request",
+		Errors:  data,
+	}
+}
+
+func NewBadRequestParameterID() *Errors {
+	return &Errors{
+		Code:    http.StatusBadRequest,
+		Message: "bad request",
+		Errors:  "error parameter id",
+	}
+}
+
+func NewFailedToConvertData() *Errors {
+	return &Errors{
+		Code:    http.StatusInternalServerError,
+		Message: "internal server error",
+		Errors:  "cannot convert id param",
+	}
+}
+
+func NewInternalServerError(data interface{}) *Errors {
+	return &Errors{
+		Code:    http.StatusInternalServerError,
+		Message: "internal server error",
+		Errors:  data,
+	}
+}
+
+func NewNotFound(data interface{}) *Errors {
+	return &Errors{
+		Code:    http.StatusNotFound,
+		Message: "not found",
+		Errors:  data,
+	}
+}
--- models/models.go
+++ models/models.go
@@ -6,6 +6,30 @@ type Motor struct {
 	Price float64 `json:"price"`
 }
 
+func (m Motor) Validate() error {
+	cErr := []string{}
+	if m.Name == "" {
+		cErr = append(cErr, "name cannot null")
+	}
+
+	if m.Price == 0.0 {
+		cErr = append(cErr, "price cannot null")
+	}
+
+	if len(cErr) > 0 {
+		return NewBadRequest(cErr)
+	}
+
+	return nil
+}
+
+func (m Motor) Exist() error {
+	if m.ID == 0 {
+		return NewNotFound("motor not found")
+	}
+	return nil
+}
+
 type Response struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
